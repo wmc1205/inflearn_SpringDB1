11cd49e6c0e4722995e2b9770c10e58f5fc007ee
Mincheol Woo <80812932+wmc1205@users.noreply.github.com>
2023-04-16 17:07:27 +0900

    transaction Aop 적용

--- src/main/java/hello/jdbc_cozil/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc_cozil/service/MemberServiceV3_3.java
@@ -2,44 +2,33 @@ package hello.jdbc_cozil.service;
 
 import hello.jdbc_cozil.domain.Member;
 import hello.jdbc_cozil.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 
 /**
- * 트랜젝션 - 트랜젝션 템플릿 / 중복 코드 제거
+ * 트랜젝션 - @Transactional AOP
+ * -트랜젝션 로직 제거
+ * -@Transactional 사용으로 비즈니스로직에 트랜젝션 설정
  */
 @Slf4j
-public class MemberServiceV3_2 {
+public class MemberServiceV3_3 {
 
 
-    //private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
+
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager,MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3( MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    //트랜젝션 설정
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        //txTemplate -> 트랜젝션 시작, 커밋, 롤백 코드가 모두 제거(생성자에서 자동 주입)
-        txTemplate.executeWithoutResult((status) -> {
-            //비즈니스 로직
-            try {
                 bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
-
     }
     //트랜젝션 로직과 비즈니스 로직 구분을 위해서 분리
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
@@ -56,7 +45,6 @@ public class MemberServiceV3_2 {
             throw new IllegalStateException("이체 중 예외 발생");
         }
     }
-
 
 
 }
